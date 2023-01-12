f4b80ab899b167ec0358bc513b51f302f02bc75c
albarracin_ale <albarracin.alejandro12@gmail.com>
2023-01-11 23:34:40 -0300

    segundo comit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Header } from "./components/Header";
 import { Carrito } from "./components/Carrito";
 import { DataProvider } from "./context/DataProvider";
 import { BrowserRouter as Router } from "react-router-dom";
-//import { Page } from "./components/Page";
+
 import "boxicons";
 import { ProductosList } from "./components/page/productos";
 
